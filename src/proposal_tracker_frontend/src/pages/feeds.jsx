3bcac6dbf62f5b6ef15bc81fcabf492395964bf1
Tiago Loureiro <19423709+tiagoicp@users.noreply.github.com>
2024-04-20 21:10:53 +0100

    finished feeds demo

--- src/proposal_tracker_frontend/src/pages/feeds.jsx
+++ src/proposal_tracker_frontend/src/pages/feeds.jsx
@@ -4,7 +4,82 @@ const Feeds = () => {
   return (
     <div>
       <Navbar></Navbar>
-      <p>This is a Feeds Page</p>
+      <div className="container mx-auto columns-3">
+        <div class="block max-w-sm rounded-lg border border-gray-200 bg-white p-6 shadow hover:bg-gray-100">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+            Proposal 129379
+          </h5>
+          <p class="font-normal text-gray-700">
+            NNS Replica Version Management
+          </p>
+          <p class="font-normal text-gray-700">
+            <strong>Status:</strong> Executed
+          </p>
+          <p class="font-normal text-gray-700">
+            <strong>Created:</strong> 08-03-2024 12:55:17
+          </p>
+        </div>
+
+        <div class="mt-6 block max-w-sm break-inside-avoid-column rounded-lg border border-gray-200 bg-white p-6 shadow hover:bg-gray-100">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+            Proposal 129378
+          </h5>
+          <p class="font-normal text-gray-700">
+            NNS Replica Version Management
+          </p>
+          <p class="font-normal text-gray-700">
+            <strong>Status:</strong> Executed
+          </p>
+          <p class="font-normal text-gray-700">
+            <strong>Created:</strong> 07-03-2024 12:55:17
+          </p>
+        </div>
+
+        <div class="mt-6 block max-w-sm break-inside-avoid-column rounded-lg border border-gray-200 bg-white p-6 shadow hover:bg-gray-100">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+            Proposal 129377
+          </h5>
+          <p class="font-normal text-gray-700">
+            NNS Replica Version Management
+          </p>
+          <p class="font-normal text-gray-700">
+            <strong>Status:</strong> Executed
+          </p>
+          <p class="font-normal text-gray-700">
+            <strong>Created:</strong> 06-03-2024 12:55:17
+          </p>
+        </div>
+
+        <div class="mt-6 block max-w-sm break-inside-avoid-column rounded-lg border border-gray-200 bg-white p-6 shadow hover:bg-gray-100">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+            Proposal 129376
+          </h5>
+          <p class="font-normal text-gray-700">
+            NNS Replica Version Management
+          </p>
+          <p class="font-normal text-gray-700">
+            <strong>Status:</strong> Executed
+          </p>
+          <p class="font-normal text-gray-700">
+            <strong>Created:</strong> 05-03-2024 12:55:17
+          </p>
+        </div>
+
+        <div class="mt-6 block max-w-sm break-inside-avoid-column rounded-lg border border-gray-200 bg-white p-6 shadow hover:bg-gray-100">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+            Proposal 129375
+          </h5>
+          <p class="font-normal text-gray-700">
+            NNS Replica Version Management
+          </p>
+          <p class="font-normal text-gray-700">
+            <strong>Status:</strong> Executed
+          </p>
+          <p class="font-normal text-gray-700">
+            <strong>Created:</strong> 04-03-2024 12:55:17
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
