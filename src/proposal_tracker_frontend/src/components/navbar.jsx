eaf7e0532b50ea25c02da613e227938c00b2f1d8
Tiago Loureiro <19423709+tiagoicp@users.noreply.github.com>
2024-04-20 09:57:29 +0100

    added basic navbar

--- src/proposal_tracker_frontend/src/components/navbar.jsx
+++ src/proposal_tracker_frontend/src/components/navbar.jsx
@@ -1,8 +1,78 @@
+import { useState } from "react";
+
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
-    <div>
-      <p>This is a Navbar Component</p>
-    </div>
+    <nav className="border-gray-200 bg-white">
+      <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
+        <a href="/" className="flex items-center space-x-3">
+          <img src="/codegov-logo.png" className="h-8" alt="Codegov Logo" />
+          <span className="self-center whitespace-nowrap text-2xl font-semibold">
+            Proposal Tracker
+          </span>
+        </a>
+        <div className="flex space-x-3 md:order-2 md:space-x-0">
+          <button
+            type="button"
+            className="rounded-lg bg-blue-700 px-4 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300"
+          >
+            Login
+          </button>
+          <button
+            id="menu"
+            data-collapse-toggle="navbar-cta"
+            type="button"
+            className="inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 md:hidden"
+            aria-controls="navbar-cta"
+            aria-expanded="false"
+            onClick={() => {
+              setShowMenu(!showMenu);
+            }}
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="h-5 w-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
+        </div>
+        <div
+          className={`${showMenu ? "" : "hidden"} w-full items-center justify-between md:order-1 md:flex md:w-auto`}
+          id="navbar-cta"
+        >
+          <ul className="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0">
+            <li>
+              <a
+                href="#"
+                className="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:p-0 md:hover:bg-transparent md:hover:text-blue-700"
+              >
+                About
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:p-0 md:hover:bg-transparent md:hover:text-blue-700"
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
